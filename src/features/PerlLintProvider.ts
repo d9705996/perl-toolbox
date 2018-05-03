113f49250dba44d2b5a111c928f137be88b284d6
David Walker <dwalker@sabio.co.uk>
2018-05-03 14:28:33 +0100

    Refactor lint method to SRP methods

--- src/features/PerlLintProvider.ts
+++ src/features/PerlLintProvider.ts
@@ -21,7 +21,6 @@ export default class PerlLintProvider {
 
     vscode.workspace.onDidOpenTextDocument(this.lint, this, subscriptions);
     vscode.workspace.onDidSaveTextDocument(this.lint, this);
-    vscode.workspace.textDocuments.forEach(this.lint, this);
   }
 
   public dispose(): void {
@@ -32,16 +31,21 @@ export default class PerlLintProvider {
 
   private lint(textDocument: vscode.TextDocument) {
     this.document = textDocument;
-    this.configuration = vscode.workspace.getConfiguration("perlcritic");
+    this.configuration = vscode.workspace.getConfiguration("perl-toolbox.lint");
+    console.log(this.configuration);
     if (textDocument.languageId !== "perl") {
       return;
     }
+    if (!this.configuration.enabled) {
+      return;
+    }
     let decoded = "";
-    let diagnostics: vscode.Diagnostic[] = [];
-    let proc = cp.spawn("perlcritic", this.getCommandOptions(), {
-      shell: true,
-      cwd: "c:\\perl64\\bin"
-    });
+
+    let proc = cp.spawn(
+      "perlcritic",
+      this.getCommandArguments(),
+      this.getCommandOptions()
+    );
     proc.stdout.on("data", (data: Buffer) => {
       decoded += data;
     });
@@ -51,35 +55,115 @@ export default class PerlLintProvider {
     });
 
     proc.stdout.on("end", () => {
-      decoded.split("\n").forEach(item => {
-        if (item) {
-          item = item.replace("~||~", "");
-          let tokens = item.split("~|~");
-
-          let range = new vscode.Range(
-            Number(tokens[1]) - 1,
-            Number(tokens[2]) - 1,
-            Number(tokens[1]) - 1,
-            300
-          );
-          let diagnostic = new vscode.Diagnostic(
-            range,
-            tokens[3],
-            vscode.DiagnosticSeverity.Error
-          );
-          diagnostics.push(diagnostic);
-        }
-        this.diagnosticCollection.set(this.document.uri, diagnostics);
-      });
+      this.diagnosticCollection.set(
+        this.document.uri,
+        this.getDiagnostics(decoded)
+      );
     });
   }
+
+  private getDiagnostics(output) {
+    let diagnostics: vscode.Diagnostic[] = [];
+    output.split("\n").forEach(violation => {
+      if (this.isValidViolation(violation)) {
+        diagnostics.push(this.createDiagnostic(violation));
+      }
+    });
+    return diagnostics;
+  }
+
+  private createDiagnostic(violation) {
+    let tokens = violation.replace("~||~", "").split("~|~");
+
+    return new vscode.Diagnostic(
+      this.getRange(tokens),
+      this.getMessage(tokens),
+      this.getSeverity(tokens)
+    );
+  }
+
+  private getRange(tokens) {
+    return new vscode.Range(
+      Number(tokens[1]) - 1,
+      Number(tokens[2]) - 1,
+      Number(tokens[1]) - 1,
+      300
+    );
+  }
+
+  private getMessage(tokens) {
+    return this.getSeverityAsText(tokens[0]).toUpperCase() + ": " + tokens[3];
+  }
+
+  private getSeverityAsText(severity) {
+    switch (parseInt(severity)) {
+      case 5:
+        return "gentle";
+      case 4:
+        return "stern";
+      case 3:
+        return "harsh";
+      case 2:
+        return "cruel";
+      default:
+        return "brutal";
+    }
+  }
+
+  private getSeverity(tokens) {
+    switch (this.configuration[this.getSeverityAsText(tokens[0])]) {
+      case "hint":
+        return vscode.DiagnosticSeverity.Hint;
+      case "info":
+        return vscode.DiagnosticSeverity.Information;
+      case "warning":
+        return vscode.DiagnosticSeverity.Warning;
+      default:
+        return vscode.DiagnosticSeverity.Error;
+    }
+  }
+
+  private isValidViolation(violation) {
+    return violation.split("~|~").length === 6;
+  }
+
   private getCommandOptions() {
+    return {
+      shell: true,
+      cwd: this.getWorkingDirectory()
+    };
+  }
+
+  private getCommandArguments() {
     return [
-      "--" + this.configuration.lintseverity,
-      "--noprofile",
+      "--" + this.getLintSeverity(),
+      this.useProfile(),
+      this.getExcludedPolicies(),
       "--verbose",
       '"%s~|~%l~|~%c~|~%m~|~%e~|~%p~||~%n"',
       this.document.fileName
     ];
+  }
+
+  private getExcludedPolicies() {
+    let policies = [];
+    this.configuration.excludedPolicies.forEach(policy => {
+      policies.push("--exclude");
+      policies.push(policy);
+    });
+    return policies.join(" ");
+  }
+
+  private getWorkingDirectory() {
+    return this.configuration.path;
+  }
+
+  private useProfile() {
+    if (!this.configuration.useProfile) {
+      return "--noprofile";
+    }
+  }
+  private getLintSeverity() {
+    return this.configuration.severity;
   }
 }
